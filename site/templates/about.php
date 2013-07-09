cd6a97de6bb71c182ccf79e8b639935ff3647a58
Francesco Improta <francesco@Seroth.local>
2013-07-09 17:37:10 +0200

    Added articles links. (#1)

--- site/templates/about.php
+++ site/templates/about.php
@@ -26,17 +26,37 @@
                 <section class="span">
                     <h3>Published Articles</h3>
                     <dl>
-                        <dt><a href="#">Interface Design</a></dt>
-                        <dd> HTML.IT <span>IT</span>
-                            <em>(April 2009)</em>
+                        <dt><a href="http://www.html.it/articoli/un-design-efficace-principi-e-proporzione-divina-1/">The divine proportion in web design</a></dt>
+                        <dd> HTML.IT | <span>italian</span>
+                            <em>(2009)</em>
                         </dd>
-                        <dt><a href="#">Create a simple tab widget</a></dt>
-                        <dd> HTML.IT <span>IT</span>
-                            <em>(April 2009)</em>
+                        <dt><a href="http://www.uxmagazine.it/user-experience-design/la-curva-dellesperienza/">The experience curve</a></dt>
+                        <dd> UXMagazine | <span>italian</span>
+                            <em>(2009)</em>
                         </dd>
-                        <dt><a href="#">CSS Solutions for IE6</a></dt>
-                        <dd> HTML.IT <span>IT</span>
-                            <em>(April 2009)</em>
+                        <dt><a href="http://www.html.it/articoli/progettare-un-prototipo-1/">How to prototype</a></dt>
+                        <dd> HTML.IT | <span>italian</span>
+                            <em>(2009)</em>
+                        </dd>
+                        <dt><a href="http://www.html.it/articoli/progettare-interfacce-unintroduzione-1/">Interface Design</a></dt>
+                        <dd> HTML.IT | <span>italian</span>
+                            <em>(2009)</em>
+                        </dd>
+                        <dt><a href="http://www.uxmagazine.it/user-experience-design/504/">Take advantage of the conventions in design</a></dt>
+                        <dd> UXMagazine | <span>italian</span>
+                            <em>(2009)</em>
+                        </dd>
+                        <dt><a href="http://www.html.it/articoli/widget-multifunzione-con-tab-1/">Create a simple tab widget</a></dt>
+                        <dd> HTML.IT | <span>italian</span>
+                            <em>(2009)</em>
+                        </dd>
+                        <dt><a href="http://www.html.it/articoli/scrivere-codice-css-due-approcci-1/">Writing efficient CSS code</a></dt>
+                        <dd> HTML.IT | <span>italian</span>
+                            <em>(2009)</em>
+                        </dd>
+                        <dt><a href="http://www.html.it/articoli/5-soluzioni-css-per-ie6-1/">CSS Solutions for IE6</a></dt>
+                        <dd> HTML.IT | <span>italian</span>
+                            <em>(2009)</em>
                         </dd>
                     </dl>
                 </section>
@@ -46,7 +66,7 @@
                     <figure><img src="<?php echo $image->url() ?>" alt="<?php echo $image->title() ?>" /> </figure>
                     <dl>
                         <dt><a href="#">CSS3 Solutions</a></dt>
-                        <dd>Apress <em>(August 2012)</em>
+                        <dd>Apress <em>(2012)</em>
                         </dd>
                     </dl>
                 </section>
